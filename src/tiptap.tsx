b0fa232dde0c944c72db1c68df68705a79a95011
Stephan Kulla <git.mail@kulla.me>
2024-07-26 00:50:04 +0200

    feat: Add button to add exercise

--- src/tiptap.tsx
+++ src/tiptap.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import { useEditor, EditorContent, FloatingMenu } from '@tiptap/react'
+import {
+  useEditor,
+  FloatingMenu,
+  EditorProvider,
+  useCurrentEditor,
+} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { Exercise } from './extensions'
+import { Button, Flex } from '@radix-ui/themes'
 
 const extensions = [StarterKit, Exercise]
 const content = '<p>Hello World!</p>'
@@ -13,9 +19,40 @@ export default function Tiptap() {
   })
 
   return (
-    <>
-      <EditorContent editor={editor} />
+    <EditorProvider
+      extensions={extensions}
+      content={content}
+      slotBefore={<Toolbar />}
+    >
       <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-    </>
+    </EditorProvider>
+  )
+}
+
+function Toolbar() {
+  const { editor } = useCurrentEditor()
+
+  if (!editor) {
+    return null
+  }
+
+  const insertExercise = () => {
+    const { $to } = editor.state.selection
+    const endOfBlock = $to.end($to.depth)
+
+    editor
+      .chain()
+      .focus()
+      .insertContentAt(endOfBlock, {
+        type: 'exercise',
+        content: [{ type: 'text', text: 'Exercise' }],
+      })
+      .run()
+  }
+
+  return (
+    <Flex gap="3">
+      <Button onClick={insertExercise}>Add Exercise</Button>
+    </Flex>
   )
 }
